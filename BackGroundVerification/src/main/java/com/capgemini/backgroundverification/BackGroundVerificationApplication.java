8079a81591689425e621f6cd4b94c8adafb1b151
marneni <marnenisindhu2@gmail.com>
2020-04-20 10:50:44 +0530

    first commit

---
+++ BackGroundVerification/src/main/java/com/capgemini/backgroundverification/BackGroundVerificationApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.backgroundverification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackGroundVerificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackGroundVerificationApplication.class, args);
+	}
+
+}
